6c54a151ebd758c4d5ee9c16dfa0835b51980afc
Guillaume Roguez <yomgui1@gmail.com>
2019-05-23 01:27:29 -0400

    Provides Docker Qt4 build instructions
    * Modify Qt5 packaging script to accept --qt4 argument for Qt4 packaging.
    
    Quick documentation to build Natron Qt4 with Docker:
    
    * dl Natron sources with this commit.
    * install Docker on your host machine and execute following commands in order:
    
    docker build -t natron-old-builder:qt4 -f tools/docker/Dockerfile.centos6-qt4 tools/docker
    
    docker run -dt --name natron-old-builder-qt4.run \
      -v $PWD:/opt/Natron-src:ro,Z \
      -v natron-sdk-centos6:/opt/Natron-sdk \
      -v natron-build-centos6:/opt/Natron-build \
      -v natron-ofx-centos6:/usr/OFX \
      -v natron-tarballs:/opt/Natron-tarballs \
      natron-old-builder:qt4
    
    docker exec -it --privileged natron-old-builder-qt4.run scl enable devtoolset-8 rh-git29 -- ./include/scripts/build-Linux-sdk.sh
    
    * Open a bash on the docker qt4 image:
    
    docker exec -it --privileged natron-old-builder-qt4.run scl enable devtoolset-8 rh-git29 -- bash
    
    * Inside this bash, type following commands to setup environment:
    
    SDK=/opt/Natron-sdk
    QTDIR="$SDK/qt4"
    FFMPEG="$SDK/ffmpeg-gpl2"
    LIBRAW="$SDK/libraw-gpl2"
    PATH="$SDK/bin:$QTDIR/bin:$GCC/bin:$FFMPEG/bin:$LIBRAW_PATH:$PATH"
    LIBRARY_PATH="$SDK/lib:$QTDIR/lib:$GCC/lib64:$GCC/lib:$FFMPEG/lib:$LIBRAW/lib"
    LD_LIBRARY_PATH="$SDK/lib:$QTDIR/lib:$GCC/lib64:$GCC/lib:$FFMPEG/lib:$LIBRAW/lib"
    LD_RUN_PATH="$SDK/lib:$QTDIR/lib:$GCC/lib:$FFMPEG/lib:$LIBRAW/lib"
    CPATH="$SDK/include:$QTDIR/include:$GCC/include:$FFMPEG/include:$LIBRAW/include"
    PYTHONHOME="$SDK"
    PYTHONPATH="$QTDIR/lib/python2.7/site-packages"
    export PKG_CONFIG_PATH="$SDK/lib/pkgconfig:$SDK/osmesa/lib/pkgconfig:$QTDIR/lib/pkgconfig:$GCC/lib/pkgconfig:$FFMPEG/lib/pkgconfig:$LIBRAW/lib/pkgconfig"
    export QTDIR LD_LIBRARY_PATH LD_RUN_PATH PKG_CONFIG_PATH CPATH SDK
    export PYTHONPATH PYTHONHOME
    
    * Now, always in the docker bash, type:
    
    cd /opt/Natron-build && mkdir natron && cd natron
    
    qmake -r $NATRON_SRC_DIR CONFIG+="enforce-gcc8 c++11 debug enable-cairo custombuild" BUILD_USER_NAME="docker"
    
    make -j$(nproc)
    
    $NATRON_SRC_DIR/tools/docker/scripts/packageNatron.sh --qt4
    
    * You can exit from the docker bash by pressing CTRL-C
    
    * Proceed as the Qt5 version to obtain the Natron package (on the host machine)
    
    docker cp natron-old-builder-qt4.run:/opt/Natron-build/Natron-snapshot ./
    
    * Run Natron:
    
    ./Natron-snapshot/bin/Natron

--- tools/docker/scripts/packageNatron.sh
+++ tools/docker/scripts/packageNatron.sh
@@ -18,6 +18,22 @@
 set -eu
 set -x
 
+case "${1:-}" in
+--qt4)
+	QT4=1
+	;;
+*)
+	# Defaults behavior is Qt5, PySide2 and Shiboken2
+	QT4=0
+	;;
+esac
+
+if [[ $QT4 == 1 ]]; then
+	PYSIDEVER=
+else
+	PYSIDEVER=2
+fi
+
 D="$NATRON_BUILD_DIR/Natron-snapshot"
 rm -rf $D
 
@@ -55,36 +71,54 @@ do
 done
 
 # Qt plugins
-for dir in $(ls "$SDK"/plugins);
+if [[ $QT4 == 1 ]]; then
+	QT_PLUGINS_DIR=$SDK/qt4/plugins/
+else
+	QT_PLUGINS_DIR=$SDK/plugins/
+fi
+
+for dir in $(ls "$QT_PLUGINS_DIR");
 do
-	cp -a "$SDK/plugins/$dir" $D/bin/
+	cp -a "$QT_PLUGINS_DIR/$dir" $D/bin/
 done
 
 # Python
 PYVER=2.7
-PY_PKG_PATH="$D/lib/python${PYVER}/site-packages/"
+PY_PKG_PATH="$SDK/lib/python${PYVER}/site-packages"
 cp -a "$SDK/lib/python${PYVER}" "$D/lib/"
-rm -rf "${PY_PKG_PATH}/shiboken2_"*
+
+if [[ $QT4 == 1 ]]; then
+	cp -a "$SDK/qt4/lib/python${PYVER}" "$D/lib/"
+fi
+
+rm -rf "${PY_PKG_PATH}/shiboken${PYSIDEVER}_"*
 rm -rf "${PY_PKG_PATH}/pip"*
 rm -rf "${PY_PKG_PATH}/setuptools"*
 rm -rf "${PY_PKG_PATH}/pkg_resources"
 rm -rf "$D/lib/python${PYVER}"/{test,config,config-"${PYVER}m"}
 
-if [[ ! -f "$SDK/lib/python27.zip" ]]; then
+PYZIPFILE="$SDK/lib/python${PYVER/./}.zip"
+if [[ ! -f "$PYZIPFILE" ]]; then
 	pushd "$SDK/lib/python${PYVER}"
-	zip -r -m "$SDK/lib/python27.zip" * -x site-packages\* lib-dynload\* lib2to3\* \
+	zip -r -m "$PYZIPFILE" * -x site-packages\* lib-dynload\* lib2to3\* \
 		lib-tk\* plat-linux2\* LICENSE.txt pdb.doc wsgiref.egg-info || true
 	popd
 fi
-cp "$SDK/lib/python27.zip" "$D/lib/"
+cp "$PYZIPFILE" "$D/lib/"
 
-LD_LIBRARY_PATH="${PY_PKG_PATH}/shiboken2:${PY_PKG_PATH}/PySide2${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+# PySide/Shiboken
+if [[ $QT4 == 1 ]]; then
+	PYSIDE_PKG_BASE="$SDK/qt4/lib/python${PYVER}/site-packages"
+	LD_LIBRARY_PATH="${PYSIDE_PKG_BASE}:${PYSIDE_PKG_BASE}/PySide${PYSIDEVER}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+else
+	LD_LIBRARY_PATH="${PY_PKG_PATH}/shiboken${PYSIDEVER}:${PY_PKG_PATH}/PySide${PYSIDEVER}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+fi
 
 # OpenColorIO
 cp "$SDK/lib/libOpenColorIO.so.1" "$D/lib/"
 
 # OpenImageIO
-cp "$SDK/lib/libOpenImageIO.so.1.8" "$D/lib/"
+cp "$SDK/lib/libOpenImageIO.so.1.8" "$SDK/lib/libOpenImageIO_Util.so.1.8" "$D/lib/"
 
 # Binary Libraries
 CORE_DEPENDS=$(ldd $(find "$D/bin" -type f) | grep /opt | awk '{print $3}' | sort -u)
@@ -106,8 +140,13 @@ do
     fi
 done
 
-PYSIDE_DEPENDS=$(ldd $(find "$SDK/lib/python${PYVER}/site-packages/PySide2" -maxdepth 1 -type f -name "lib*.so*") | grep /opt | awk '{print $3}' | sort -u)
-PYSIDE_DEPENDS+=$(ldd $(find "$SDK/lib/python${PYVER}/site-packages/shiboken2" -maxdepth 1 -type f -name "lib*.so*") | grep /opt | awk '{print $3}' | sort -u)
+if [[ $QT4 == 1 ]]; then
+	PYSIDE_DEPENDS=$(ldd $(find "$SDK/qt4/lib/python${PYVER}/site-packages/PySide${PYSIDEVER}" -maxdepth 1 -type f -name "*.so") | grep /opt | awk '{print $3}' | sort -u)
+	PYSIDE_DEPENDS+=$(ldd "$SDK/qt4/lib/python${PYVER}/site-packages/shiboken.so" | grep /opt | awk '{print $3}' | sort -u)
+else
+	PYSIDE_DEPENDS=$(ldd $(find "$SDK/lib/python${PYVER}/site-packages/PySide${PYSIDEVER}" -maxdepth 1 -type f -name "lib*.so*") | grep /opt | awk '{print $3}' | sort -u)
+	PYSIDE_DEPENDS+=$(ldd $(find "$SDK/lib/python${PYVER}/site-packages/shiboken${PYSIDEVER}" -maxdepth 1 -type f -name "lib*.so*") | grep /opt | awk '{print $3}' | sort -u)
+fi
 for i in $PYSIDE_DEPENDS; do
     dep=$(basename "$i")
     if [ ! -f "$D/lib/$dep" ]; then
